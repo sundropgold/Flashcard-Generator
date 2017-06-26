38164705799c5ce6af4cf9251cef7fe2d2af8126
Flynn <flynnigan.tan@gmail.com>
2017-06-26 18:34:00 -0500

    added inquirer

--- main.js
+++ main.js
@@ -4,7 +4,13 @@ var BasicCard = require("./BasicCard.js");
 // get cloze card constructor
 var ClozeCard = require("./ClozeCard.js");
 
-///////////////////////////////////////////////
+// inquirer
+var inquirer = require("inquirer");
+
+// store flashcards here
+var usersFlashcards = [];
+
+// TEST ////////////////////////////////////////
 
 var firstPresident = new BasicCard("Who was the first president of the United States?", "George Washington");
 
@@ -27,3 +33,63 @@ console.log(firstPresidentCloze.fullText);
 
 // Should throw or log an error because "oops" doesn't appear in "This doesn't work"
 var brokenCloze = new ClozeCard("This doesn't work", "oops");
+
+//////////////////////////////////////////////////
+
+inquirer.prompt([
+
+	{
+		type:"list",
+		message:"Do you want to make a basic flashcard or a cloze-deleted flashcard?",
+		choices:["basic", "cloze-deleted"],
+		name:"cardtype"
+	
+	}
+	]).then(function(user){
+
+		if (user.cardtype === "basic") {
+
+			inquirer.prompt([
+
+					{
+						type:"input",
+						message:"Insert your flashcard's question here.",
+						name:"question"
+					},
+					{
+						type:"input",
+						message:"Insert your flashcard's answer here.",
+						name:"answer"
+					}
+				]).then(function(basic){
+
+					var flashCard = new BasicCard(basic.question, basic.answer);
+
+					usersFlashcards.push(flashCard);
+
+				});
+
+		}
+
+		else if (user.cardtype === "cloze-deleted") {
+
+			inquirer.prompt([
+
+				{
+					type:"input",
+					message:"Insert your flashcard's full text here.",
+					name:"text"	
+				},
+				{	
+					type:"input",
+					message:"Insert your flashcard's cloze-deletion here.",
+					name:"cloze"
+				}
+				]).then(function(cd){
+
+					var flashCard = new ClozeCard(cd.text, cd.cloze);
+	
+					usersFlashcards.push(flashCard);
+				});
+		}
+	});
